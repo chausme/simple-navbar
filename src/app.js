052b5dc52757a80d810823f92e116e27d470fefc
Alex Chaus <chausme@gmail.com>
2022-11-11 15:34:49 +0700

    Add navbar transform

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
-navToggle.addEventListener('click', () => {
-    links.classList.toggle('links-collapsed');
+navToggle.addEventListener('click', e => {
+    links.classList.toggle('collapsed');
+    let button = e.target;
+    if (e.target.nodeName === 'I') {
+        button = e.target.parentElement;
+    }
+    button.classList.toggle('collapsed');
 });
